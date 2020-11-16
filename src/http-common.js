faa6a16bfa1361ba40f76d6e3fac418349229b44
kevin <kcv0330@gmail.com>
2020-11-16 08:56:20 -0600

    cambiar url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8083/api/v1",
+    baseURL: "https://webdev-backend.herokuapp.com/api/v1/",
     headers: {
         "Content-type": "application/json"
     }
